808e03dbce038d99d78d7a4e0c8b75822630404d
SimModer <sandronino96@gmail.com>
2025-02-04 22:13:52 +0400

    Delete scripts.js

--- scripts.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Example of dynamically loading content
-    document.getElementById("content").innerHTML = "<h2>Welcome to Shikimori Clone</h2>";
-});
