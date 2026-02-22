80c311b5c223397c5f65b01cbdb4e923017cb85b
SimModer <sandronino96@gmail.com>
2026-02-22 20:10:06 +0400

    database update

--- db/parsing.py
+++ db/parsing.py
@@ -46,7 +46,7 @@ else:
     last_page = 1
 
 # Запрос кол-ва страниц
-i = int(input("Введите количество страниц (468 anime, 884 manga, 160 ranobe): "))
+i = int(input("Введите количество страниц (469 anime, 887 manga, 160 ranobe): "))
 
 for page in range(last_page, i + 1):
     url = url_template.format(page)
